3bc60d5da6503fe430e9011066bb2ba7204716f8
Jake Weinstein <jake@aospa.co>
2020-03-14 00:29:40 +0100

    sepolicy: qcom: Allow applications to check for camera properties
    Change-Id: Ie79ba1cc2b59fa0ea27f4a0c8417beb9820bcfa5

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -13,11 +13,23 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/pa/sepolicy/qcom/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/pa/sepolicy/qcom/dynamic \
-    device/pa/sepolicy/qcom/system-only
+    BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+        device/pa/sepolicy/qcom/system-only
+    ifeq ($(filter true,$(TARGET_USES_QCOM_LEGACY_PRE_UM_SEPOLICY) $(TARGET_USES_QCOM_LEGACY_UM_SEPOLICY)), true)
+    BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+        device/pa/sepolicy/qcom/dynamic-legacy
+    else
+    BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+        device/pa/sepolicy/qcom/dynamic
+    endif
 else
-BOARD_SEPOLICY_DIRS += \
-    device/pa/sepolicy/qcom/dynamic \
-    device/pa/sepolicy/qcom/vendor
+    BOARD_SEPOLICY_DIRS += \
+        device/pa/sepolicy/qcom/vendor
+    ifeq ($(filter true,$(TARGET_USES_QCOM_LEGACY_PRE_UM_SEPOLICY) $(TARGET_USES_QCOM_LEGACY_UM_SEPOLICY)), true)
+    BOARD_SEPOLICY_DIRS += \
+        device/pa/sepolicy/qcom/dynamic-legacy
+    else
+    BOARD_SEPOLICY_DIRS += \
+        device/pa/sepolicy/qcom/dynamic
+    endif
 endif
